9ce91b4df026144d024d7f780e95cc43060b6282
ChloneCraft <YOUREMAIL@example.com>
2023-11-18 01:41:45 +0100

    created basic button comp

---
+++ src/components/AddArgumentButton.tsx
@@ -0,0 +1,10 @@
+export default function AddArgumentButton() {
+  return (
+    <div>
+      <button>
+        Add <br />
+        Argument
+      </button>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import Textbox from "@/components/Textbox";
+import AddArgumentButton from "@/components/AddArgumentButton";
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
       </Head>
       <main>
         <section className="ProArgumentsContainer">
-          <button>ADD</button>
+          <AddArgumentButton />
         </section>
         <section className="ContraArgumentsContainer"></section>
       </main>
